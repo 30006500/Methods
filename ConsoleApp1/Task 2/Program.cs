922e55605ccb99ebc93c554d55940f19dfbbbb97
Campbell McChesney <30006500@g.toiohomai.ac.nz>
2018-07-30 11:51:04 +1200

    First Commit

---
+++ ConsoleApp1/Task 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("Area Calculator");
+                Console.WriteLine("1) Enter Circle Values");
+                Console.WriteLine("2) Calculate Circle Area");
+                Console.WriteLine("3) Enter Square Values");
+                Console.WriteLine("4) Calculate Circle Area");
+                Console.WriteLine("5) Exit Program");
+                Console.WriteLine("Please enter the number of your selection");
+                choice = int.Parse(Console.ReadLine());
+                if (choice > 5) Console.WriteLine("{0} is not a valid choice", choice);
+
+                switch (choice)
+                {
+                    case 1:
+                        Circle.Values();
+                        break;
+                    case 2:
+                        Circle.Area();
+                        break;
+                    case 3:
+                        Square.Values();
+                        break;
+                    case 4:
+                        Square.Area();
+                        break;
+                    default:
+                        break;
+                }
+            } while (choice != 5);
+        }
+    }
+}
---
+++ ConsoleApp1/Task1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+            Console.WriteLine("1) Add");
+            Console.WriteLine("2) Subtract");
+            Console.WriteLine("3) Multiply");
+            Console.WriteLine("4) Divide");
+            Console.WriteLine("Please enter the number of your choice");
+            int calc = int.Parse(Console.ReadLine());
+
+            
+
+
+            Console.WriteLine("Please enter the first number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            switch (calc)
+            {
+                case 1:
+                    Console.WriteLine("{0} + {1} = {2}", num1, num2, Math.AddNums(num1, num2));
+                    break;
+                case 2:
+                    Console.WriteLine("{0} - {1} = {2}", num1, num2, Math.SubtractNums(num1, num2));
+                    break;
+                case 3:
+                    Console.WriteLine("{0} x {1} = {2}", num1, num2, Math.MultiplyNums(num1, num2));
+                    break;
+                case 4:
+                    Console.WriteLine("{0} / {1} = {2}", num1, num2, Math.DivideNums(num1, num2));
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+
+            
+            Console.ReadLine();
+        }
+
+        public static class Math
+        {
+            public static int MultiplyNums(int _num1, int _num2)
+            {
+                return _num1 * _num2;
+            }
+
+            public static int AddNums(int _num1, int _num2)
+            {
+                return _num1 + _num2;
+            }
+
+            public static int DivideNums(int _num1, int _num2)
+            {
+                return _num1 / _num2;
+            }
+
+            public static int SubtractNums(int _num1, int _num2)
+            {
+                return _num1 - _num2;
+            }
+        }
+    }
+}
